ccaf410698d8279a7ffba2f625c03759b329c344
Nathan Daniel Peterson <natperson@gmail.com>
2017-12-14 14:45:25 -0800

    add text to screen with button submit

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 
+class Greetings extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { emoji: ['Hello'] }
+    }
 
-const greeting = 'Greetings';
+    clickHandler = (e) => {
+        e.preventDefault()
+        console.log(e.target.input.value)
+        this.setState({emoji: [e.target.input.value, ...this.state.emoji]})
+    }
 
-class Greetings extends Component{
-  constructor(props){
-    super(props)
-  }
-
-  render(){
-    return (
-      <div>
-        <div>
-          { greeting }
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return ( < form onSubmit = { this.clickHandler } >
+            <input name="input" />
+            <div > { this.state.emoji } </div>  
+            <button > Submit </button>  
+            </form>
+        )
+    }
 }
 
-export default Greetings;
+export default Greetings
